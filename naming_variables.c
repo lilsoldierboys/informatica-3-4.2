7c2ddc476fe0e713aa3daeff09edfcfe48787e0a
Cesar Franco <cesarantoniofranco14@gmail.com>
2026-04-21 21:40:59 +0000

    variables

---
+++ naming_variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int number;//dont need = marks in this stupid language
+    int testvalue;
+    int example_1;
+    float Cesar;
+    printf("Variables created successfully.\n");
+}
+//this is a comment
