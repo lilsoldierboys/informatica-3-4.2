daef0958a3584e019f7c552d1eef63eff68f1537
Cesar Franco <cesarantoniofranco14@gmail.com>
2026-04-24 14:48:36 +0000

    input

--- hello.c
+++ hello.c
@@ -2,7 +2,15 @@
 
 int main(void)
 {
-    printf("Hello Cesar \n");
+    char user_name[50];
+    printf("What is your name? ");
+    scanf("%s", user_name);
+    printf("Hello %s\n",user_name);
+    char color[20];
+    printf("what is your favorite color? ");
+    scanf("%s",color);
+    printf("%s is a stupid color\n",color);
+    
 }
 
 
