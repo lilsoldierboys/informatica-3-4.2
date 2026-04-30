3edf81bc9844a270f7406df77d4a167a11a6e064
Cesar Franco <cesarantoniofranco14@gmail.com>
2026-04-30 15:36:30 +0000

    dumb time

--- Week2/min.c
+++ Week2/min.c
@@ -5,21 +5,32 @@ int main(void){
     int start_t_m;
     int end_t_h;
     int end_t_m;
+    int new_h_t;
+    int new_m_t;
     printf("How long is the movie in minutes? \n");
     scanf("%d",&mov_run_t);
     printf("What is the starting time? \n");
-    scanf("%d""%d",&start_t_h,&start_t_m);
-    int min_t = mov_run_t/60;
-
-    printf("The movie is %d hours and long\n", min_t);
-
-    end_t_h = mov_run_t;
-    end_t_m = mov_run_t%60;
+    scanf("%d:%d", &start_t_h, &start_t_m);
+    int hour_t = mov_run_t/60;
+    int min_t_m = mov_run_t%60;
+    printf("The movie is %d hours and %d minutes long\n", hour_t,min_t_m);
 
 
 
-    printf("%d",end_t_h);
-    //printf("The movie will end at %d:%d \n",end_t_h,end_t_m);
+
+
+
+
+    new_h_t= start_t_h + hour_t;
+    new_m_t= start_t_m + min_t_m;
+
+    
+
+
+
+
+
+    printf("The movie will end at %d:%02d \n",new_h_t,new_m_t);
 
 
 
